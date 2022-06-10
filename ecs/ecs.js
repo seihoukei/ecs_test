d42df71b7d51852931769c71a64f4d542066e51f
seihoukei <a@zefiris.su>
2022-06-10 23:09:08 +0300

    Basics

---
+++ ecs/ecs.js
@@ -0,0 +1,161 @@
+export default class ECS {
+    static #createId() {
+        return crypto.randomUUID()
+    }
+    
+    static #createComponent(type, data) {
+        const component = Object.create(null)
+        Object.assign(component, data)
+        component.__type = type
+        return component
+    }
+    
+    // main storage, map of entities with sets of components
+    #entities = new Map()
+    
+    // search storage for entity with given component
+    #glossary = new Map()
+    
+    #addEntityToStorage(entity, data = new Set()) {
+        this.#entities.set(entity, data)
+    }
+    
+    #getEntityFromStorage(entity) {
+        return this.#entities.get(entity)
+    }
+    
+    #removeEntityFromStorage(entity) {
+        this.#entities.delete(entity)
+    }
+    
+    #getComponentGlossary(type, create = false) {
+        let componentGlossary = this.#glossary.get(type)
+        if (!componentGlossary && create) {
+            componentGlossary = new Map()
+            this.#glossary.set(type, componentGlossary)
+        }
+        return componentGlossary
+    }
+    
+    #getEntityFromComponentGlossary(type, entity, create = false) {
+        const componentGlossary = this.#getComponentGlossary(type, create)
+        let components = componentGlossary.get(entity)
+        if (!components && create) {
+            components = new Set()
+            componentGlossary.set(entity, components)
+        }
+        return components
+    }
+    
+    #addComponentToStorage(component, entity) {
+        const components = this.#getEntityFromStorage(entity)
+        components.add(component)
+        component.__entity = entity
+        
+        return component
+    }
+    
+    #removeComponentFromStorage(component) {
+        const entityComponents = this.getEntityComponents(component.__entity)
+        entityComponents.delete(component)
+    }
+
+    #addComponentToGlossary(component) {
+        const list = this.#getEntityFromComponentGlossary(component.__type, component.__entity, true)
+        list.add(component)
+    }
+    
+    #removeComponentFromGlossary(component) {
+        const componentGlossary = this.#getComponentGlossary(component.__type)
+        const components = componentGlossary.get(component.__entity)
+        components.delete(component)
+        if (components.size === 0) {
+            componentGlossary.delete(component.__entity)
+            if (componentGlossary.size === 0)
+                this.#glossary.delete(component.__type)
+        }
+    }
+    
+    createEntity() {
+        const entity = ECS.#createId()
+        this.#addEntityToStorage(entity)
+        return entity
+    }
+    
+    loadEntity(entity, componentsData) {
+        const components = new Set(componentsData)
+        this.#addEntityToStorage(entity, components)
+    }
+    
+    deleteEntity(entity) {
+        const components = this.#getEntityFromStorage(entity)
+        components.forEach(component => {
+            this.removeComponent(component)
+        })
+        this.#removeEntityFromStorage(entity)
+    }
+    
+    getEntityComponents(entity) {
+        return this.#getEntityFromStorage(entity)
+    }
+    
+    getEntityComponentsByType(entity, type) {
+        return this.#getEntityFromComponentGlossary(type, entity)
+    }
+    
+    addComponent(entity, type, data) {
+        const component = ECS.#createComponent(type, data)
+        
+        this.#addComponentToStorage(component, entity)
+        this.#addComponentToGlossary(component)
+    }
+    
+    removeComponent(component) {
+        this.#removeComponentFromGlossary(component)
+        this.#removeComponentFromStorage(component)
+    }
+    
+    removeComponentsByType(entity, type) {
+        const componentGlossary = this.#getComponentGlossary(type)
+        const components = componentGlossary.get(entity)
+        if (components) {
+            components.forEach(component => {
+                this.removeComponent(component)
+            })
+        }
+    }
+    
+    saveData() {
+    
+    }
+    
+    loadData(data) {
+    
+    }
+    
+    debug_dump() {
+        const result = {
+            entities : {},
+            glossary : {}
+        }
+        
+        this.#entities.forEach((entityComponents, entityId) => {
+            result.entities[entityId] = {}
+            entityComponents.forEach(component => {
+                result.entities[entityId][component.__type] = component
+            })
+        })
+        
+        this.#glossary.forEach((components, type) => {
+            result.glossary[type] = {}
+            components.forEach((components, entity) => {
+                result.glossary[type][entity] = {}
+                components.forEach(component => {
+                    result.glossary[type][entity][component.__type] = component
+                })
+            })
+        })
+        
+        return result
+    }
+}
--- example/scripts/index.js
+++ example/scripts/index.js
@@ -1,2 +1,26 @@
+import ECS from "../../ecs/ecs.js"
+
 window.onload = () => {
+    const base = new ECS()
+
+    function createCreep(x, y) {
+        const creep = base.createEntity()
+        base.addComponent(creep, 'position', { x, y })
+        base.addComponent(creep, 'velocity', { x: 0, y: 0 })
+        base.addComponent(creep, 'size', { width: 0, height: 0 })
+        base.addComponent(creep, 'color', { r: 0, g: 0, b: 0, a: 0 })
+        return creep
+    }
+
+    const a = createCreep(0, 0)
+    const b = createCreep(20, 20)
+    
+    console.log(base.debug_dump())
+
+    base.removeComponentsByType(a, 'position')
+    base.removeComponentsByType(b, 'position')
+    base.removeComponentsByType(a, 'velocity')
+    
+    console.log(base.debug_dump())
+    debugger
 }
